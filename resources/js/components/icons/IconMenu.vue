4d06a0e8cb8c470cfe0e36994d281aa71bb19602
heyosseus <ratiruxadzee@gmail.com>
2024-03-11 21:01:54 +0400

    style: add images and icons

---
+++ resources/js/components/icons/IconMenu.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" >
+        <path
+            d="M2 6h20M2 12h20m-20 6h20"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+    </svg>
+</template>
