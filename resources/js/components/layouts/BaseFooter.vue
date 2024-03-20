636d424abc8a6c3be4ada4dc97fa1114751de228
heyosseus <ratiruxadzee@gmail.com>
2024-03-20 16:15:56 +0400

    feat: configure footer component

---
+++ resources/js/components/layouts/BaseFooter.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer>
+        <div class="bg-primary py-16 ">
+            <p class="text-center text-white">© 2024, GorillazNFT, All rights reserved.</p>
+            <p class="text-primary text-center text-sm">Developed by Heyosseus</p>
+        </div>
+    </footer>
+
+
+</template>
